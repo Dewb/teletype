351f7bf1b7239138045d425feba1e688bb338fe2
Michael Dewberry <michael.dewberry@gmail>
2021-10-02 10:51:47 -0400

    Add fragment deserialization test

--- tests/serialize_scene_tests.c
+++ tests/serialize_scene_tests.c
@@ -9,6 +9,7 @@
 #include "state.h"
 #include "serializer.h"
 #include "serialize.h"
+#include "ops\op_enum.h"
 
 void test_file_write_buffer(void* user_data, uint8_t* buffer, uint16_t size) {
     fwrite(buffer, 1, size, (FILE*)user_data);
@@ -27,18 +28,78 @@ bool test_file_eof(void* user_data) {
     return feof((FILE*)user_data) != 0;
 }
 
-tt_serializer_t test_writer;
-tt_deserializer_t test_reader;
+typedef struct {
+    char* buffer;
+    unsigned int length;
+    unsigned int position;
+} stringsource;
+
+void test_string_write_buffer(void* user_data, uint8_t* buffer, uint16_t size)
+{
+    stringsource* ss = (stringsource*)user_data;
+    strncpy(ss->buffer + ss->position, (char*)buffer, size);
+    ss->position += size;
+    ss->length += size;
+}
+void test_string_write_char(void* user_data, uint8_t c)
+{
+    stringsource* ss = (stringsource*)user_data;
+    ss->buffer[ss->position] = c;
+    ss->position += 1;
+    ss->length += 1;
+}
+uint16_t test_string_read_char(void* user_data)
+{
+    stringsource* ss = (stringsource*)user_data;
+    if (ss->position < ss->length)
+    {
+        char r = ss->buffer[ss->position];
+        ss->position += 1;
+        return r;
+    }
+    else
+    {
+        return -1;
+    }
+}
+bool test_string_eof(void* user_data)
+{
+    stringsource* ss = (stringsource*)user_data;
+    return (ss->position >= ss->length);
+}
+
+tt_serializer_t test_file_writer, test_string_writer;
+tt_deserializer_t test_file_reader, test_string_reader;
 
 void init_streams()
 {
-    test_writer.write_buffer = &test_file_write_buffer;
-    test_writer.write_char = &test_file_write_char;
-    test_writer.print_dbg = &test_print_dbg;
+    test_file_writer.write_buffer = &test_file_write_buffer;
+    test_file_writer.write_char = &test_file_write_char;
+    test_file_writer.print_dbg = &test_print_dbg;
 
-    test_reader.read_char = &test_file_read_char;
-    test_reader.eof = &test_file_eof;
-    test_reader.print_dbg = &test_print_dbg;
+    test_file_reader.read_char = &test_file_read_char;
+    test_file_reader.eof = &test_file_eof;
+    test_file_reader.print_dbg = &test_print_dbg;
+
+    test_string_writer.write_buffer = &test_string_write_buffer;
+    test_string_writer.write_char = &test_string_write_char;
+    test_string_writer.print_dbg = &test_print_dbg;
+
+    test_string_reader.read_char = &test_string_read_char;
+    test_string_reader.eof = &test_string_eof;
+    test_string_reader.print_dbg = &test_print_dbg;
+}
+
+void deserialize_fragment(char* fragment, scene_state_t* scene, char (*text)[SCENE_TEXT_LINES][SCENE_TEXT_CHARS])
+{
+    stringsource ss;
+    ss.buffer = fragment;
+    ss.length = strlen(fragment);
+    ss.position = 0;
+
+    test_string_reader.data = (void*)&ss;
+
+    deserialize_scene(&test_string_reader, scene, text);
 }
 
 int compare_files(char* filename, FILE* a, FILE* b) {
@@ -79,18 +140,45 @@ TEST test_round_trip_preset(char* filename, char* tempfile)
     {
         FILE* infile = fopen(filename, "rb");
         ASSERT(infile != 0);
-        test_reader.data = (void*)infile;
+        test_file_reader.data = (void*)infile;
 
-        deserialize_scene(&test_reader, &scene, &text);
+        deserialize_scene(&test_file_reader, &scene, &text);
 
         FILE* outfile = fopen(tempfile, "w+b");
         ASSERT(outfile != 0);
-        test_writer.data = (void*)outfile;
+        test_file_writer.data = (void*)outfile;
 
-        serialize_scene(&test_writer, &scene, &text);
+        serialize_scene(&test_file_writer, &scene, &text);
 
         CHECK_CALL(compare_files(filename, infile, outfile));
     }
+
+    PASS();
+}
+
+TEST test_basic_deserialize_script()
+{
+    scene_state_t scene;
+    ss_init(&scene);
+
+    char text[SCENE_TEXT_LINES][SCENE_TEXT_CHARS];
+    memset(text, 0, SCENE_TEXT_LINES * SCENE_TEXT_CHARS);
+
+    deserialize_fragment("#1\nTR.P 4\n\n", &scene, &text);
+    // confirm we parsed a command with OP and NUMBER
+    ASSERT(scene.scripts[0].c[0].length == 2);
+    ASSERT(scene.scripts[0].c[0].data[0].tag == OP);
+    ASSERT(scene.scripts[0].c[0].data[0].value == E_OP_TR_P);
+    ASSERT(scene.scripts[0].c[0].data[1].tag == NUMBER);
+    ASSERT(scene.scripts[0].c[0].data[1].value == 4);
+    // confirm only one command in script
+    ASSERT(scene.scripts[0].c[1].length == 0);
+    // confirm rest of scripts are empty
+    for (int i = 1; i < 11; i++) {
+        ASSERT(scene.scripts[i].c[0].length == 0);
+    }
+    // confirm no text
+    ASSERT(text[0][0] == 0);
 
     PASS();
 }
@@ -108,5 +196,6 @@ SUITE(serialize_scene_suite) {
     // RUN_TESTp(test_round_trip_preset, "../presets/tt06.txt", "./results/tt06.txt");
     // RUN_TESTp(test_round_trip_preset, "../presets/tt07.txt", "./results/tt07.txt");
     RUN_TESTp(test_round_trip_preset, "../presets/tt08.txt", "./results/tt08.txt");
+    RUN_TEST(test_basic_deserialize_script);
     log_print();
 }
